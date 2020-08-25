1870aab0b4bf013ea68407ca641606125a4297a3
Mike Stillman <mikestillman1@gmail.com>
2020-08-25 10:08:39 -0400

    trajectory output files now created

---
+++ src-new/examples.hpp
@@ -0,0 +1,20 @@
+#include "Polynomial.hpp"
+#include "PolynomialFDS.hpp"
+
+PolynomialFDS examplePDS1();
+
+Polynomial examplePoly1();
+Polynomial examplePoly2();
+Polynomial examplePoly3();
+
+void someTests();
+
+PolynomialFDS examplePDS2();
+PolynomialFDS cyclonePDS1();
+PolynomialFDS cyclonePDS2();
+PolynomialFDS cyclonePDS3();
+
+
+
+
+
--- src-new/main.cpp
+++ src-new/main.cpp
@@ -1,9 +1,9 @@
-// create image file from a doot file as like:
+// create image file from a dot file as like:
 //   dot -Tpng -o foo.png foo-statespace.dot
 // TODO for June 2020
 //   1. DONE: write a function readFDS().  DONE.
 //   2. DONE: incorporate that into `main`, also printing to a file. WORKING ON THIS. Write limit cycle to a file. Use states, not indices.
-//   3. DONE: Write a dot file.
+//   3. DONE: Write a dot file. DONE
 //   4. test this on some real examples, that we can test "by hand", or "by M2", or by cyclone.
 //   5. create a number of tests (at least 20 examples, with at least the summary answers, some with dot files)
 //   6. try to incorporate this into algorun and plantsimlab.
@@ -18,193 +18,6 @@
 #include <unordered_map>
 #include "Polynomial.hpp"
 #include "PolynomialFDS.hpp"
-
-PolynomialFDS examplePDS1()
-{
-// MODEL NAME: eg2 (unused)
-// SIMULATION NAME: eg2 (unused)
-// NUMBER OF VARIABLES: 3
-// VARIABLE NAMES: x1 x2 x3 (unused, sort of)
-// NUMBER OF STATES: 2 2 2 (must all be the same, a prime number)
-// SPEED OF VARIABLES: 1 1 1  (unused)
-
-// f1 = 1+x2
-// f2 = x1+x3
-// f3 = x1*x2
-
-  std::vector<std::string> varnames { "x1", "x2", "x3" };
-  std::vector<Polynomial> polys;
-  // loc0 = 0
-  // loc1 = 1
-  // loc2 = x1
-  // loc3 = x2
-  // loc4 = x3
-  // Create polynomial f1
-  int nstates = 101;
-  Polynomial f1(nstates, 3); // 2 states, 3 vars
-  f1.createPlusNode(1,nstates+2-1);
-  polys.push_back(f1);
-  // Create polynomial f2
-  Polynomial f2(nstates, 3);
-  f2.createPlusNode(nstates+1-1,nstates+3-1);
-  polys.push_back(f2);
-  // Create polynomial f3
-  Polynomial f3(nstates, 3);
-  f3.createTimesNode(nstates+1-1,nstates+2-1);
-  polys.push_back(f3);
-
-  return PolynomialFDS(polys, varnames);
-}
-
-Polynomial examplePoly1()
-{
-  Polynomial f(3, 4); // 3 states, 4 variables 0,1,2,  3,4,5,6
-  int n1 = f.createPlusNode(3,4);
-  int n2 = f.createPlusNode(5,n1);
-  int n3 = f.createTimesNode(n1,n2);
-  f.createTimesNode(n3, 1);
-  f.debug_display(std::cout);
-  return f;
-}
-
-Polynomial examplePoly2()
-{
-  Polynomial g(3, 4);
-  int n1 = g.createPlusNode(1,1);
-  int n2 = g.createPowerNode(n1,2);
-  g.createPlusNode(5,n2);
-  g.debug_display(std::cout);
-  return g;
-}
-
-Polynomial examplePoly3()
-{
-  Polynomial h(3, 4);
-  h.createPlusNode(1,1);
-  h.debug_display(std::cout);
-  return h;
-}
-
-void someTests()
-{
-  std::vector<std::string> varnames = { "x3", "x", "P53" };
-  Polynomial f = parsePolynomial(varnames, 3, "x3 + P53*( 1 + x)");
-  std::cout << f.evaluateSymbolic(varnames) << std::endl;
-
-  varnames = { "x3", "x", "P53", "AB_3", "AB_1", "AB4" };
-  f = parsePolynomial(varnames, 3, "x3 + P53*( 1 + x)^ 5*  AB_3");
-  std::cout << f.evaluateSymbolic(varnames) << std::endl;
-
-  varnames = { "x3", "x", "P53", "AB_3", "AB_1", "AB4" };
-  f = parsePolynomial(varnames, 3, "x3 + P53*( 1 + x)^ 4*  AB_3");
-  std::cout << f.evaluateSymbolic(varnames) << std::endl;
-
-  f = parsePolynomial(varnames, 3, "x3 + x^3 + (2+1)*P53 + AB_3^6");
-  std::cout << f.evaluateSymbolic(varnames) << std::endl;
-
-  f = parsePolynomial(varnames, 5, "x3 + x^3 + (2+1)*P53 + AB_3^6");
-  std::cout << f.evaluateSymbolic(varnames) << std::endl;
-
-  f = parsePolynomial(varnames, 5, "1+(2*5)^2+3");
-  std::cout << f.evaluateSymbolic(varnames) << std::endl;
-}
-
-PolynomialFDS examplePDS2()
-{
-  int nstates = 2;
-  std::vector<std::string> varnames { "x1", "x2", "x3" };
-  std::vector<std::string> polystrs
-    {
-     "x1+x2",
-     "x2*x3+1",
-     "x1+x1*x3"
-    };
-
-  std::vector<Polynomial> polys;
-  for (const auto& s : polystrs)
-    polys.push_back(parsePolynomial(varnames, nstates, s));
-  return PolynomialFDS(polys, varnames);
-}
-
-#if 0  
-  f = examplePoly1();
-  int pt[4] {0,1,1,2};
-  std::cout << "f(0,1,1,2) = " << f.evaluate(pt) << std::endl;
-  std::cout << f.evaluateSymbolic({"x1","x3","x5","x7"}) << std::endl;
-
-  Polynomial g = examplePoly2();
-  Polynomial h = examplePoly3();
-
-  PolynomialFDS pds = examplePDS1();
-  std::cout << pds << std::endl;
-  std::vector<long> stateSpace = computeStateSpace(pds);
-  //  for (auto a : stateSpace) {
-  //    std::cout << a << " ";
-  //  }
-  
-  if (argc < 2)
-    {
-      std::cout << "simFDS <FDS file> -o <output dot file>" << std::endl;
-      return 1;
-    }
-  return 0;
-}
-#endif
-
-PolynomialFDS cyclonePDS1()
-{
-  int nstates = 2;
-  std::vector<std::string> varnames { "x1", "x2" };
-  std::vector<std::string> polystrs
-    {
-     "1+x2",
-     "0+x1" // TODO: BUG: replacing this with x1 causes the poly to become 0!
-    };
-
-  std::vector<Polynomial> polys;
-  for (const auto& s : polystrs)
-    polys.push_back(parsePolynomial(varnames, nstates, s));
-  return PolynomialFDS(polys, varnames);
-}
-
-PolynomialFDS cyclonePDS2()
-{
-  int nstates = 2;
-  std::vector<std::string> varnames { "x1", "x2", "x3" };
-  std::vector<std::string> polystrs
-    {
-     "1+x2",
-     "x1+x3",
-     "x1*x2"
-    };
-
-  std::vector<Polynomial> polys;
-  for (const auto& s : polystrs)
-    polys.push_back(parsePolynomial(varnames, nstates, s));
-  return PolynomialFDS(polys, varnames);
-}
-
-PolynomialFDS cyclonePDS3()
-{
-  int nstates = 2;
-  std::vector<std::string> varnames { "M", "B", "Ob", "Tb", "A", "Da", "L", "Hl" };
-  std::vector<std::string> polystrs
-    {
-     "A",
-     "M + (B*(Tb+1)) + M*B*(Tb+1)",
-     "(M+1)*B",
-     "(M+1)*Ob",
-     "(B*L + Hl + B*L*Hl) + A*Da*(B+1) + (B*L + Hl + B*L*Hl)*A*Da*(B+1)",
-     "((B+1) + (L+1) + (B+1)*(L+1))*(Hl+1)*A",
-     "L",
-     "Hl"
-    };
-
-  std::vector<Polynomial> polys;
-  for (const auto& s : polystrs)
-    polys.push_back(parsePolynomial(varnames, nstates, s));
-  return PolynomialFDS(polys, varnames);
-}
 
 void runStateSpaceComputation(std::string projectName, bool write_dot_file)
 {
@@ -227,6 +40,59 @@ void runStateSpaceComputation(std::string projectName, bool write_dot_file)
     }
 }
 
+  // TODO: display.
+  //  length of the trajectory until it hits the limit cycle (summary)
+  //  length of the limit cycle (summary)
+  //  the limit cycle itself (dot file/summary)
+  //  entire graph of the whole trajectory (dot file).
+
+void writeTrajectoryGraph(std::ostream& o,
+                            int numStates,
+                            int numVariables,
+                            const std::vector<long>& trajectory,
+                            long finalIndex)
+{
+  o << "digraph trajectory {" << std::endl;
+
+  // display the edges
+  State u(numStates, numVariables);
+  State v(numStates, numVariables);
+  for (long i=0; i < trajectory.size(); ++i)
+    {
+      u.setFromIndex(trajectory[i]);
+      if (i < trajectory.size() - 1)
+        v.setFromIndex(trajectory[i+1]);
+      else
+        v.setFromIndex(trajectory[finalIndex]);
+      o << "  "
+        << "\"" << u << "\""
+        << " -> "
+        << "\"" << v << "\""
+        << std::endl;
+    }
+
+  o << "}" << std::endl;
+}
+
+void writeTrajectorySummary(std::ostream& o,
+                            int numStates,
+                            int numVariables,
+                            const std::vector<long>& trajectory,
+                            long finalIndex // index into 'trajectory'
+                            )
+{
+  long cycleLength = trajectory.size() - finalIndex;
+  if (cycleLength == 1)
+    {
+      o << "#steps to the fixed point: " << finalIndex << std::endl;
+    }
+  else
+    {
+      o << "#steps to enter the limit cycle: " << finalIndex << std::endl;
+      o << "length of the limit cycle: " << cycleLength << std::endl;
+    }
+}
+
 void runTrajectoryComputation(std::string projectName, const std::vector<int>& startPoint, bool write_dot_file)
 {
   // TODO: get this code working.
@@ -237,13 +103,13 @@ void runTrajectoryComputation(std::string projectName, const std::vector<int>& s
 
   if (startPoint.size() != pds->numVariables())
     {
-      // TO DO: ERROR
+      std::cerr << "Expected trajectory start state of length " << pds->numVariables() << std::endl;
+      exit(1);
     }
   // TODO:check that all entries of startPoint are in range 0..pds.numState()-1
 
   std::vector<long> trajectory;
-  std::unordered_map<long, long> hash;
-
+  std::unordered_map<long, long> hash; //encoded state => position in the trajectory.
   
   State u(pds->numStates(), startPoint); // TODO: write this constructor.
   State v(pds->numStates(), pds->numVariables());
@@ -257,7 +123,7 @@ void runTrajectoryComputation(std::string projectName, const std::vector<int>& s
       pds->evaluate(u.getState(), v.getState());
       long vindex = v.getIndex();
       auto result = hash.insert(std::make_pair(vindex,count));
-      if (not result.second)
+      if (not result.second) // if the state v already has been seen on this trajectory
         {
           final_index = hash[vindex];
           break;
@@ -266,15 +132,14 @@ void runTrajectoryComputation(std::string projectName, const std::vector<int>& s
       count++;
     }
 
-  std::cout << "count = " << count << std::endl;
-  std::cout << "final_index = " << final_index << std::endl;
-  std::cout << u << std::endl;
-  std::cout << v << std::endl;
-  // TODO: display.
-  //  length of the trajectory until it hits the limit cycle (summary)
-  //  length of the limit cycle (summary)
-  //  the limit cycle itself (dot file/summary)
-  //  entire graph of the whole trajectory (dot file).
+  writeTrajectorySummary(std::cout, pds->numStates(), pds->numVariables(), trajectory, final_index);
+  if (write_dot_file)
+    {
+      std::ofstream ofil;
+      ofil.open(projectName + "-trajectory.dot");
+      writeTrajectoryGraph(ofil, pds->numStates(), pds->numVariables(), trajectory, final_index);
+      ofil.close();
+    }
 }
 
 int main(int argc, char* argv[])
@@ -328,12 +193,6 @@ int main(int argc, char* argv[])
           trajectoryStart.push_back(val);
         }
     }
-  
-  std::cout << "trajectory index: " << trajectoryIndex << std::endl;
-  std::cout << "summary index: " << summaryIndex << std::endl;
-  std::cout << "trajectory: ";
-  for (auto a : trajectoryStart) std::cout << a << " ";
-  std::cout << std::endl;
   
   bool write_dot_file = (summaryIndex == -1);
 
